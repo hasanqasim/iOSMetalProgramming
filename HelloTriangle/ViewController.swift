af1125a362c5b6ae333de71090b101e599a7b415
Hasan Qasim <hasanqasim92@gmail.com>
2020-10-25 23:56:23 +1100

    create MTLDevice, add CAMetalLayer and set a vertex buffer

--- HelloTriangle/ViewController.swift
+++ HelloTriangle/ViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import Metal
+import QuartzCore
 
 class ViewController: UIViewController {
-
+    
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    var vertexBuffer: MTLBuffer! = nil
+    
+    let vertexData: [Float] = [
+        0.0, 0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        device = MTLCreateSystemDefaultDevice()
+        initCAMetalLayer()
+        
+    }
+    
+    func initCAMetalLayer() {
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+    }
+    
+    func setVertexBuffer() {
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: .storageModePrivate)
+        
     }
 
 
