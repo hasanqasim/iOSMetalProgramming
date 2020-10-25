0cc946274a748813f104f977e959765b85887d60
Hasan Qasim <hasanqasim92@gmail.com>
2020-10-25 23:22:00 +1100

    Initial Commit

---
+++ HelloTriangle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloTriangle
+//
+//  Created by Hasan Qasim on 25/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
